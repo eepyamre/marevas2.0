b0f7971d4ade8aaa839e9d25d0eb9b9b3349be40
eepyamre <succiezi@mail.ru>
2024-02-28 21:26:17 +0300

    chrome fix

--- src/app/input/inputController.ts
+++ src/app/input/inputController.ts
@@ -4,7 +4,6 @@ import { Core } from "../core";
 export class InputController {
   shouldDraw = false;
   moveCanvas = false;
-  cursor: HTMLDivElement;
   constructor() {
     Core.appRoot.addEventListener("pointerdown", this.pointerdown);
     Core.appRoot.addEventListener("pointermove", this.pointermove);
@@ -12,35 +11,27 @@ export class InputController {
     Core.appRoot.addEventListener("pointerout", this.pointerup);
     Core.appRoot.addEventListener("contextmenu", (e) => e.preventDefault());
     Core.appRoot.addEventListener("wheel", this.zoom);
-    this.cusorInit();
-  }
-  private cusorInit() {
-    this.cursor = document.querySelector("#cursor")!;
-    if (!this.cursor) {
-      throw new Error("No cursor found!");
-    }
   }
   private pointerdown = (e: PointerEvent) => {
     e.preventDefault();
-    this.cursor.style.background = "#121212";
+    Core.bufferController.drawingCanvasEl.setPointerCapture(e.pointerId);
     if (e.buttons === 4) {
       this.moveCanvas = true;
       this.shouldDraw = false;
       return;
     }
+    console.log(e);
     Core.bufferController.startDraw(
-      new Vector2(e.layerX, e.layerY),
+      new Vector2(e.offsetX, e.offsetY),
       e.pointerType === "pen" ? e.pressure : 1
     );
     this.shouldDraw = true;
   };
   private pointermove = (e: PointerEvent) => {
     e.preventDefault();
-    this.cursor.style.left = e.clientX + "px";
-    this.cursor.style.top = e.clientY + "px";
     if (this.shouldDraw && e.buttons)
       Core.bufferController.draw(
-        new Vector2(e.layerX, e.layerY),
+        new Vector2(e.offsetX, e.offsetY),
         e.pointerType === "pen" ? e.pressure : 1
       );
     if (this.moveCanvas) {
@@ -52,7 +43,7 @@ export class InputController {
   };
   private pointerup = (e: PointerEvent) => {
     e.preventDefault();
-    this.cursor.style.background = "transparent";
+    Core.bufferController.drawingCanvasEl.releasePointerCapture(e.pointerId);
     if (this.shouldDraw) {
       Core.bufferController.endDraw();
       this.shouldDraw = false;
